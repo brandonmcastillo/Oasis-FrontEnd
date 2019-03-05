c6d9723a34894cad0e69bfb4826b6de3dd6ca917
brandonmcastillo <brandonmaerina@gmail.com>
2019-03-05 00:19:16 -0800

    Add Test Sign in and Sign up form to root page / Created Routes folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Info from './components/Home/Info'
 import SignUpForm from './components/Forms/SignUpForm'
+import LoginForm from './components/Forms/LoginForm'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Info />
         <SignUpForm />
+        <LoginForm />
       </div>
     );
   }
