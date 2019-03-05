28c3064e124c7fb91fe7ba40fc1ea642bd123ffe
brandonmcastillo <brandonmaerina@gmail.com>
2019-03-04 21:08:59 -0800

    Using React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/Home/Info.css'
+
+
 
 class App extends Component {
   render() {
---
+++ src/components/Home/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Info.css'
+class Info extends Component {
+  render() {
+    return (
+      <div>
+       
+      </div>
+    )
+  }
+}
+
+export default Info;
