6910f02332a8842f14334ad414547650f2e034d6
awang617 <wanga18@mail.wlu.edu>
2019-03-06 14:19:36 -0800

    wrapped forms in a modal

--- src/components/Forms/SignUpForm.js
+++ src/components/Forms/SignUpForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Col, InputGroup } from 'react-bootstrap';
+import { Form, Col, InputGroup, Modal, Button } from 'react-bootstrap';
 import './Form.css';
 class SignUpForm extends Component {
   constructor(...args) {
@@ -20,95 +20,114 @@ class SignUpForm extends Component {
   render() {
     const { validated } = this.state;
     return (
-      <Form
-        className="Form"
-        noValidate
-        validated={validated}
-        onSubmit={e => this.handleSubmit(e)}
-      >
-        <Form.Row>
-          <Form.Group as={Col} md="12" controlId="validationCustom01">
-            <Form.Label>Name</Form.Label>
-            <Form.Control required type="text" placeholder="Name" />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="12" controlId="validationCustomUsername">
-            <Form.Label>Username</Form.Label>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-              </InputGroup.Prepend>
-              <Form.Control
-                name="username"
-                onChange={this.props.handleInput}
-                type="text"
-                placeholder="Username"
-                aria-describedby="inputGroupPrepend"
+      <Modal 
+        show={this.props.show} 
+        onHide={this.props.onHide}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header>
+          <Modal.Title>
+            Sign up
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form
+            className="Signup-Form"
+            noValidate
+            validated={validated}
+            onSubmit={e => this.handleSubmit(e)}
+          >
+            <Form.Row>
+              <Form.Group as={Col} md="12" controlId="validationCustom01">
+                <Form.Label>Name</Form.Label>
+                <Form.Control required type="text" placeholder="Name" />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="12" controlId="validationCustomUsername">
+                <Form.Label>Username</Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control
+                    name="username"
+                    onChange={this.props.handleInput}
+                    type="text"
+                    placeholder="Username"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a username.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="12" controlId="validationCustom01">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  required
+                  name="password"
+                  onChange={this.props.handleInput}
+                  type="password"
+                  placeholder="Password"
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="12" controlId="validationCustom01">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  required
+                  name="email"
+                  onChange={this.props.handleInput}
+                  type="text"
+                  placeholder="Email"
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="12" controlId="validationCustom03">
+                <Form.Label> Favorite City</Form.Label>
+                <Form.Control
+                  name="city"
+                  onChange={this.props.handleInput}
+                  type="text"
+                  placeholder="City"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid city.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Group>
+              <Form.Check
                 required
+                label="Agree to terms and conditions"
+                feedback="You must agree before submitting."
               />
-              <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="12" controlId="validationCustom01">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              required
-              name="password"
-              onChange={this.props.handleInput}
-              type="password"
-              placeholder="Password"
+            </Form.Group>
+            <input
+              value="Submit"
+              type="submit"
+              className="btn-primary"
+              onClick={this.props.handleSignUp}
             />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="12" controlId="validationCustom01">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              required
-              name="email"
-              onChange={this.props.handleInput}
-              type="text"
-              placeholder="email"
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="12" controlId="validationCustom03">
-            <Form.Label> Favorite City</Form.Label>
-            <Form.Control
-              name="city"
-              onChange={this.props.handleInput}
-              type="text"
-              placeholder="City"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid city.
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-        <Form.Group>
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-          />
-        </Form.Group>
-        <input
-          value="Submit"
-          type="submit"
-          className="btn-primary"
-          onClick={this.props.handleSignUp}
-        />
-      </Form>
+          </Form>
+
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
