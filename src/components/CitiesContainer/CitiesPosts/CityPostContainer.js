d2b692fd9c97c502be09d8574f3224f4499b1244
awang617 <wanga18@mail.wlu.edu>
2019-03-06 17:01:54 -0800

    small updates

--- src/components/CitiesContainer/CitiesPosts/CityPostContainer.js
+++ src/components/CitiesContainer/CitiesPosts/CityPostContainer.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 import CityHeader from "./CityHeader";
 import PostContainer from "./PostContainer";
 
 class CityPostContainer extends Component {
+  
   render() {
     return (
       <div>
--- src/components/CitiesContainer/CitiesPosts/PostContainer.js
+++ src/components/CitiesContainer/CitiesPosts/PostContainer.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import CreatePost from './CreatePost'
 import Post from "./Post";
 
 class PostContainer extends Component {
+  state = {
+    modalShow: false,
+  }
   render() {
+    let modalClose = () => this.setState({ modalShow: false });
+
     let posts = this.props.posts.map((post, idx) => {
       return <Post post={post.content} key={idx} />;
     });
     return (
       <div>
         <h1>Posts</h1>
+        <Button onClick={() => this.setState({modalShow: true})}>Create Post</Button>
+        <CreatePost show={this.state.modalShow} onHide={modalClose}/>
         {posts}
       </div>
     );
