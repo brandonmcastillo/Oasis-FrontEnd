0e0aa589ac40918812f3f5fdaa646e8f39ed90b9
Michael_Claus <mclausaudio@gmail.com>
2019-03-10 14:19:34 -0700

    most recent posts display on top

--- src/components/Profile/PostList.js
+++ src/components/Profile/PostList.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import Post from './Post.js'
-import './Post.css'
+import React, { Component } from "react";
+import Post from "./Post.js";
+import "./Post.css";
 class PostList extends Component {
-    render() {
-        let userPosts=this.props.userPosts.map( (post) => {
-            return <Post 
-                    key={post._id} 
-                    postId={post._id} 
-                    title={post.title}
-                    city={post.cityId.name} 
-                    content={post.content}
-                    editPost={this.props.editPost}
-                    deletePost={this.props.deletePost}/>
-        })
-        return (
-            <div>
-                {userPosts}
-            </div>
-        );
-    }
+  render() {
+    let userPosts = this.props.userPosts.reverse().map(post => {
+      return (
+        <Post
+          key={post._id}
+          postId={post._id}
+          title={post.title}
+          city={post.cityId.name}
+          content={post.content}
+          editPost={this.props.editPost}
+          deletePost={this.props.deletePost}
+        />
+      );
+    });
+    return <div>{userPosts}</div>;
+  }
 }
 
 export default PostList;
